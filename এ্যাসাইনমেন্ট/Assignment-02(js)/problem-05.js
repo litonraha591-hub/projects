d149f460878fda03518d0a6d64a2bb639a5d78ad
litonraha <litonraha591@gmail.com>
2026-07-31 12:24:26 +0600

    final submission

--- এ্যাসাইনমেন্ট/Assignment-02(js)/problem-05.js
+++ এ্যাসাইনমেন্ট/Assignment-02(js)/problem-05.js
@@ -15,7 +15,7 @@ let newArr=[];
   
  if ((Array.isArray(times) === true) && (times.length !== 0) && (newArr.length !== 0)){ 
 let total =0;
-for(i = 0; i<times.length; i++){
+for(let i = 0; i<times.length; i++){
   total +=times[i]
 }
 let averageTime = total/times.length;
@@ -28,9 +28,9 @@ let averageTime = total/times.length;
 }
 
 
-console.log(averageResponseTime("hi"));
-console.log(averageResponseTime([1,"5",10]));
-console.log(averageResponseTime([120, 200, 150, 130]));
-console.log(averageResponseTime([]));
-console.log(averageResponseTime([100,100]));
-console.log(averageResponseTime([10,10,10]));
+// console.log(averageResponseTime("hi"));
+// console.log(averageResponseTime([1,"5",10]));
+// console.log(averageResponseTime([120, 200, 150, 130]));
+// console.log(averageResponseTime([]));
+// console.log(averageResponseTime([100,100]));
+// console.log(averageResponseTime([10,10,10]));
